c9832b73a1800a82bc2ea570b4729da6093e9645
NTC <trongcong96@gmail.com>
2023-02-11 23:41:36 +0700

    update

--- page-listings.php
+++ page-listings.php
@@ -18,66 +18,57 @@ get_header(); ?>
 				while ( have_posts() ) :
 					the_post();
 					the_content();
+					$query = create_query_barista();
 					?>
-                    <div class="__listings-wrap">
-                        <div class="__lt-filter-top">
-                            <button type="button" class="__show-filter-popup">
-                                <span></span>
-                                <span class="sr-only">Filters</span>
-                                <span class="up-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" data-name="Layer 1" viewBox="0 0 14 14" role="img">
-                                        <path d="M0 2.48v2h2.09a3.18 3.18 0 006.05 0H14v-2H8.14a3.18 3.18 0 00-6.05 0zm3.31 1a1.8 1.8 0 111.8 1.81 1.8 1.8 0 01-1.8-1.82zm2.2 6.29H0v2h5.67a3.21 3.21 0 005.89 0H14v-2h-2.29a3.19 3.19 0 00-6.2 0zm1.3.76a1.8 1.8 0 111.8 1.79 1.81 1.81 0 01-1.8-1.79z"></path>
-                                    </svg>
-                                </span>
-                                <span>Filters</span>
-                            </button>
-                        </div>
-						<div class="__lt-inner-wrap">
-							<div class="__lt-filter-side">
-                                <div class="__lt-by">Filter by</div>
-                                <div class="__lt-filter-side-inner">
-                                    <?php lt_filter_group() ?>
-                                </div>
-                            </div>
-							<div class="__lt-content-side">
-								<div class="__lt-items-wrap">
-                                    <?php
-                                    $query = create_query_barista();
-                                    if ( $query->have_posts() ) {
-	                                    while ( $query->have_posts() ) {
-		                                    $query->the_post();
-		                                    get_lt_item( get_the_ID() );
-	                                    }
-	                                    // Restore original Post Data
-	                                    wp_reset_postdata();
-                                    } else {
-	                                    echo '<div class="__lt-item item-not-found">Sorry, no barista matched your criteria.</div>';
-                                    }
-                                    ?>
-								</div>
-							</div>
-						</div>
-                        <div class="__lt-filter-modal" style="display: none">
-                            <div class="__lt-modal-backdrop"></div>
-                            <div class="__filter-popup-content">
-                                <div class="__filter-popup-inner">
-                                    <div class="__lt-by">Filter by
-                                    <button type="button" class="__lt-modal-close">
-                                        <span class="sr-only">Close the dialog</span>
+                    <div class="__lt-filter-content">
+                        <div class="__listings-wrap">
+                            <div class="__lt-filter-top">
+                                <div class="__lt-filter-button-wrap">
+                                    <button type="button" class="__show-filter-popup">
+                                        <span></span>
+                                        <span class="sr-only">Filters</span>
                                         <span class="up-icon">
-                                            <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" viewBox="0 0 14 14" role="img">
-                                                <polygon fill-rule="evenodd" points="12.524 0 7 5.524 1.476 0 0 1.476 5.524 7 0 12.524 1.476 14 7 8.476 12.524 14 14 12.524 8.476 7 14 1.476"></polygon>
+                                            <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" data-name="Layer 1" viewBox="0 0 14 14" role="img">
+                                                <path d="M0 2.48v2h2.09a3.18 3.18 0 006.05 0H14v-2H8.14a3.18 3.18 0 00-6.05 0zm3.31 1a1.8 1.8 0 111.8 1.81 1.8 1.8 0 01-1.8-1.82zm2.2 6.29H0v2h5.67a3.21 3.21 0 005.89 0H14v-2h-2.29a3.19 3.19 0 00-6.2 0zm1.3.76a1.8 1.8 0 111.8 1.79 1.81 1.81 0 01-1.8-1.79z"></path>
                                             </svg>
                                         </span>
+                                        <span>Filters</span>
                                     </button>
+                                    <span class="__counter-result"></span>
+                                </div>
+                            </div>
+                            <div class="__lt-inner-wrap">
+                                <div class="__lt-filter-side">
+                                    <div class="__lt-by">
+                                        Filter by
+                                        <span class="__counter-result">
+                                            <?= $query->found_posts ? "(" . ( $query->found_posts . " barista" ) . ( $query->found_posts > 1 ? "s)" : ")" ) : ""; ?>
+                                        </span>
                                     </div>
-                                    <div class="__filter-popup-group">
+                                    <div class="__lt-filter-side-inner">
                                         <?php lt_filter_group() ?>
                                     </div>
                                 </div>
+                                <div class="__lt-content-side">
+                                    <div class="__lt-items-wrap">
+                                        <?php
+                                        if ( $query->have_posts() ) {
+	                                        while ( $query->have_posts() ) {
+		                                        $query->the_post();
+		                                        get_lt_item( get_the_ID() );
+	                                        }
+	                                        // Restore original Post Data
+	                                        wp_reset_postdata();
+                                        } else {
+	                                        echo '<div class="__lt-item item-not-found">Sorry, no barista matched your criteria.</div>';
+                                        }
+                                        ?>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-					</div>
+						<?php lt_filter_modal(); ?>
+                    </div>
 				<?php
 				endwhile;
 			}
--- single-barista.php
+++ single-barista.php
@@ -6,9 +6,20 @@
 //}else {
 //$_SESSION['barista_view'] = 1;
 //}
+global $post;
+//current_user_can( 'barista' ) &&
+$is_author_can_edit = is_user_logged_in() && $post->post_author == get_current_user_id();
+$is_edit_barista    = isset( $_GET['edit'] ) && $is_author_can_edit;
+$title              = "THANK YOU FOR SEEING MY PROFILE";
+if ( $is_edit_barista ) {
+	acf_form_head();
+	$title = "UPDATE PROFILE";
+}
+get_header();
 
-get_header(); ?>
-<?php do_action( 'ocean_before_content_wrap' ); ?>
+do_action( 'ocean_before_content_wrap' );
+lt_add_page_header( $title );
+?>
 <div id="content-wrap" class="container clr">
 		<?php do_action( 'ocean_before_primary' ); ?>
     <div id="primary" class="content-area clr">
@@ -24,121 +35,135 @@ get_header(); ?>
 					$phone_number = get_field( "your_phone_number" );
 					$email        = get_field( "your_email" );
 					$avatar       = get_field( "your_avatar" );
-					?>
-                    <div class="__lt-single-barista">
-                        <div class="__wrap">
-                            <div class="__head-wrap">
-                                <div class="__avatar">
-                                    <img src="<?= ! empty( $avatar ) ? wp_get_attachment_image_url( $avatar['ID'] ) : 'https://via.placeholder.com/120x120.png' ?>" alt="avatar">
-                                </div>
-                                <div class="__info-wrap">
-                                    <div class="__info-left">
-                                        <div class="__name"><?= get_field( "full_name" ) ?></div>
-	                                    <?php
-	                                    $certification = get_certification_by_barista( get_the_ID() );
-	                                    if ( ! empty( $certification ) ) {
-		                                    foreach ( $certification as $cer ) {
-			                                    echo '<div class="__exp">🏅' . $cer . '</div>';
-		                                    }
-	                                    }
-	                                    ?>
+					if ( $is_edit_barista ) { ?>
+                        <div class="__lt-single-update">
+                            <?php acf_form( array(
+	                            'post_id'      => get_the_ID(),
+	                            'return'       => get_the_permalink(),
+	                            'uploader'     => 'basic',
+	                            'submit_value' => "Update profile",
+                            ) ); ?>
+                        </div>
+					<?php } else { ?>
+                        <div class="__lt-single-barista">
+                            <div class="__wrap">
+                                <?= $is_author_can_edit ? '<a class="__edit-profile" href="' . get_the_permalink() . '?edit" title="Edit profile">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 700">
+                                      <path d="M355.6 252.56a154.47 154.47 0 0 1-85.68 25.76c-31.36 0-61.04-9.52-85.68-25.76a107.64 107.64 0 0 0-92.96 106.96v69.44c0 6.16 5.04 11.2 11.2 11.2H310.8l127.12-127.12c-15.12-31.92-45.92-54.88-82.32-60.48zM378 121.52c0 59.38-48.14 107.52-107.52 107.52-59.38 0-107.52-48.14-107.52-107.52C162.96 62.14 211.1 14 270.48 14 329.86 14 378 62.14 378 121.52m-44.8 351.12c-1.68 1.68-2.24 3.36-2.8 5.04l-10.64 56c-1.12 7.28 5.04 13.44 11.76 11.76l56-10.64c2.24-.56 3.92-1.12 5.04-2.8l10.64-10.64L343.84 462zm37.79-37.57 125.52-125.52 59.8 59.8-125.53 125.51zM604.8 301.84l-41.44-41.44a13.19 13.19 0 0 0-18.48 0l-21.84 21.84 59.36 59.92 21.84-21.84a12.7 12.7 0 0 0 .56-18.48z" />
+                                    </svg>
+                                </a>' : '' ?>
+                                <div class="__head-wrap">
+                                    <div class="__avatar">
+                                        <img src="<?= ! empty( $avatar ) ? wp_get_attachment_image_url( $avatar['ID'] ) : 'https://via.placeholder.com/120x120.png' ?>" alt="avatar">
                                     </div>
-                                    <div class="__info-right">
-                                        <?= $phone_number ? '<div class="__contact-item"><span>Contacted:</span> <a href="tel:' . $phone_number . '">' . $phone_number . '</a></div>' : '' ?>
-                                        <?= $email ? '<div class="__contact-item"><span>Email me at:</span> <a href="mailto:' . $email . '">' . $email . '</a></div>' : '' ?>
+                                    <div class="__info-wrap">
+                                        <div class="__info-left">
+                                            <div class="__name"><?= get_field( "full_name" ) ?></div>
+	                                        <?php
+	                                        $certification = get_certification_by_barista( get_the_ID() );
+	                                        if ( ! empty( $certification ) ) {
+		                                        foreach ( $certification as $cer ) {
+			                                        echo '<div class="__exp">🏅' . $cer . '</div>';
+		                                        }
+	                                        }
+	                                        ?>
+                                        </div>
+                                        <div class="__info-right">
+                                            <?= $phone_number ? '<div class="__contact-item __tell"><a href="tel:' . $phone_number . '">' . $phone_number . '</a></div>' : '' ?>
+                                            <?= $email ? '<div class="__contact-item __email"><a href="mailto:' . $email . '">' . $email . '</a></div>' : '' ?>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="__content">
-                                <div class="__ct-left">
-                                    <h3 class="__label">View profile</h3>
-                                    <div class="__ct-info-item __ct-analytics">
-                                        <div>This month, I have been</div>
-                                        <div class="__analytics-wrap">
-                                            <div class="__seen">
-                                                <strong>Seen <?= get_barista_view( get_the_ID() ); ?> times</strong>
+                                <div class="__content">
+                                    <div class="__ct-left">
+                                        <h3 class="__label">View profile</h3>
+                                        <div class="__ct-info-item __ct-analytics">
+                                            <div>This month, I have been</div>
+                                            <div class="__analytics-wrap">
+                                                <div class="__seen">
+                                                    <strong>Seen <?= get_barista_view( get_the_ID() ); ?> times</strong>
+                                                </div>
+                                                <div class="__contacted">
+                                                    <strong>Contacted <?= get_barista_contacted( get_the_ID() ); ?>
+                                                        times</strong>
+                                                </div>
                                             </div>
-                                            <div class="__contacted">
-                                                <strong>Contacted <?= get_barista_contacted( get_the_ID() ); ?>
-                                                    times</strong>
+                                        </div>
+	                                    <?php if ( get_field( "preferred_name" ) ) { ?>
+                                            <div class="__ct-info-item __ct-preferred-name">
+                                                <h4>Preferred Name</h4>
+                                                <div><?= get_field( "preferred_name" ) ?></div>
+                                            </div>
+	                                    <?php } ?>
+                                        <div class="__ct-info-item __ct-year-exp">
+                                            <h4>Barista Experience (Years)</h4>
+                                            <div><?= get_field( "years_of_experience" )['label'] ?></div>
+                                        </div>
+                                        <div class="__ct-info-item __ct-year-exp-aus">
+                                            <h4>Retail or Hospitality Experience</h4>
+                                            <div><?= get_field( "experience_in_australia" )['label'] ?></div>
+                                        </div>
+                                        <div class="__ct-info-item __ct-barista-skills">
+                                            <h4>Barista Skills</h4>
+                                            <div>
+                                                <?php
+                                                $barista_skills = get_field( 'barista_skills' );
+                                                if ( $barista_skills ): ?>
+                                                    <ul>
+                                                        <?php foreach ( $barista_skills as $skill ): ?>
+                                                            <li><?= $skill; ?></li>
+                                                        <?php endforeach; ?>
+                                                    </ul>
+                                                <?php endif; ?>
+                                            </div>
+                                        </div>
+                                        <div class="__ct-info-item __ct-volume">
+                                            <h4>Barista Skills</h4>
+                                            <div>
+                                                <?= get_field( 'volume_you_are_able_to_handle_solo' ); ?>
+                                            </div>
+                                        </div>
+                                        <div class="__ct-info-item __ct-hospitality-skills">
+                                            <h4>Hospitality Skills</h4>
+                                            <div>
+                                                <?php
+                                                $hospitality_skills = get_field( 'hospitality_skills' );
+                                                if ( $hospitality_skills ): ?>
+                                                    <ul>
+                                                        <?php foreach ( $hospitality_skills as $skill ): ?>
+                                                            <li><?= $skill; ?></li>
+                                                        <?php endforeach; ?>
+                                                    </ul>
+                                                <?php endif; ?>
+                                            </div>
+                                        </div>
+                                        <div class="__ct-info-item __ct-your_resume">
+                                            <h4>Resume</h4>
+                                            <div>
+                                                <?php
+                                                $resume = get_field( 'your_resume' );
+                                                if ( $resume ): ?>
+                                                    <a target="_blank" href="<?php echo $resume['url']; ?>"><?php echo $resume['filename']; ?></a>
+                                                <?php else:
+	                                                echo "No Resume";
+                                                endif;
+                                                ?>
                                             </div>
                                         </div>
                                     </div>
-	                                <?php if ( get_field( "preferred_name" ) ) { ?>
-                                        <div class="__ct-info-item __ct-preferred-name">
-                                            <h4>Preferred Name</h4>
-                                            <div><?= get_field( "preferred_name" ) ?></div>
-                                        </div>
-	                                <?php } ?>
-                                    <div class="__ct-info-item __ct-year-exp">
-                                        <h4>Years Of Experience</h4>
-                                        <div><?= get_field( "years_of_experience" )['label'] ?></div>
-                                    </div>
-                                    <div class="__ct-info-item __ct-year-exp-aus">
-                                        <h4>Experience in Australia</h4>
-                                        <div><?= get_field( "experience_in_australia" )['label'] ?></div>
-                                    </div>
-                                    <div class="__ct-info-item __ct-barista-skills">
-                                        <h4>Barista Skills</h4>
-                                        <div>
-                                            <?php
-                                            $barista_skills = get_field( 'barista_skills' );
-                                            if ( $barista_skills ): ?>
-                                                <ul>
-                                                    <?php foreach ( $barista_skills as $skill ): ?>
-                                                        <li><?= $skill; ?></li>
-                                                    <?php endforeach; ?>
-                                                </ul>
-                                            <?php endif; ?>
-                                        </div>
-                                    </div>
-                                    <div class="__ct-info-item __ct-volume">
-                                        <h4>Barista Skills</h4>
-                                        <div>
-                                            <?= get_field( 'volume_you_are_able_to_handle_solo' ); ?>
-                                        </div>
-                                    </div>
-                                    <div class="__ct-info-item __ct-hospitality-skills">
-                                        <h4>Hospitality Skills</h4>
-                                        <div>
-                                            <?php
-                                            $hospitality_skills = get_field( 'hospitality_skills' );
-                                            if ( $hospitality_skills ): ?>
-                                                <ul>
-                                                    <?php foreach ( $hospitality_skills as $skill ): ?>
-                                                        <li><?= $skill; ?></li>
-                                                    <?php endforeach; ?>
-                                                </ul>
-                                            <?php endif; ?>
-                                        </div>
-                                    </div>
-                                    <div class="__ct-info-item __ct-your_resume">
-                                        <h4>Resume</h4>
-                                        <div>
-                                            <?php
-                                            $resume = get_field( 'your_resume' );
-                                            if ( $resume ): ?>
-                                                <a target="_blank" href="<?php echo $resume['url']; ?>"><?php echo $resume['filename']; ?></a>
-                                            <?php else:
-	                                            echo "No Resume";
-                                            endif;
-                                            ?>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="__ct-right">
-                                    <div class="__ct-des"><?= get_field( "describe_yourself_in_2_sentences" ) ?></div>
-                                    <div class="__ct-video">
-                                        <h4 class="__video-title">Watch me in action</h4>
-                                        <div class="__video-iframe">
-                                            <iframe width="1417" height="537" src="https://www.youtube.com/embed/<?= get_video_url( get_the_ID() ) ?>" title="Watch me in action" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                    <div class="__ct-right">
+                                        <div class="__ct-des"><?= get_field( "describe_yourself_in_2_sentences" ) ?></div>
+                                        <div class="__ct-video">
+                                            <h4 class="__video-title">Watch me in action</h4>
+                                            <div class="__video-iframe">
+                                                <iframe width="1417" height="537" src="https://www.youtube.com/embed/<?= get_video_url( get_the_ID() ) ?>" title="Watch me in action" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-				<?php
+					<?php }
 				endwhile;
 			}
 			?>
